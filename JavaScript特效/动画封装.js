318e125a57a8f26554877ebd6e678c3eab5756af
Elvis.Chan <meetelvischan@gmail.com>
2017-08-10 22:42:52 +0800

    JavaScript特效01

--- JavaScript特效/动画封装.js
+++ JavaScript特效/动画封装.js
@@ -1,3 +1,18 @@
 /**
  * Created by elvis on 2017/8/10.
  */
+
+
+function animate(ele,target){
+    clearInterval(ele.timer);
+    var speed = target>ele.offsetLeft?10:-10;
+    ele.timer = setInterval(function(){
+        ele.style.left = ele.offsetLeft +speed + "px";
+        var dis =target - ele.offsetLeft;
+        if(Math.abs(dis)<Math.abs(speed)){
+            ele.style.left = target + "px";
+            clearInterval(ele.timer);
+        }
+    },10)
+
+}
